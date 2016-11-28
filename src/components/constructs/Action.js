e9218f986e6339d75e78fa1d89abbd96fb12aa93
Trent West <Trent.West@misys.com>
2016-11-28 17:01:21 -0600

    added unweighted random actions

--- src/components/constructs/Action.js
+++ src/components/constructs/Action.js
@@ -4,17 +4,23 @@ import Condition from './Condition'
 class Action {
   constructor (actionArray) {
     this.actionArray = actionArray
+    console.log(this)
   }
 
   performActions () {
-    this.actionArray.forEach(function (subAction) {
-      let actionCondition = new Condition(subAction.condition)
-      if (actionCondition.checkConditions()) {
-        performableActions[subAction.type](subAction.target)
-      }
-    })
+    if (Array.isArray(this.actionArray)) {
+      this.actionArray.forEach(this.performSubaction)
+    } else {
+      this.performSubaction(this.actionArray)
+    }
   }
 
+  performSubaction (subAction) {
+    let actionCondition = new Condition(subAction.condition)
+    if (actionCondition.checkConditions()) {
+      performableActions[subAction.type](subAction.target)
+    }
+  }
 }
 
 const performableActions = {
@@ -35,6 +41,13 @@ const performableActions = {
   },
   'displayText': function (target) {
     store.commit('appendText', target)
+  },
+  'randomAction': function (target) {
+    let randAction
+    if (!target.weighted) {
+      randAction = new Action(target.possibleActions[Math.floor(Math.random() * target.possibleActions.length)])
+    }
+    randAction.performActions()
   }
 }
 
